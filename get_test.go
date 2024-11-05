607606477616bb0681e44598c2b09dfc2a6b2ee1
April Hall <ari@arithefirst.com>
2024-11-05 00:04:19 -0500

    Add unit testing for Get

---
+++ get_test.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"regexp"
+	"testing"
+)
+
+func TestGet(t *testing.T) {
+	res, err := Get("https://icanhazip.com", make(map[string]string))
+	if err != nil {
+		t.Fatal("Test failed with unexpected error: ", err)
+	}
+
+	if len(res) < 100 {
+		t.Error("Test failed: Response too short (<100 char)")
+	}
+
+	matchHttps, err := regexp.MatchString(`[h,t,p,s]{4,5}\:\/\/`, string(res))
+	if err != nil {
+		t.Fatal("Regexp crashed with unexpected error: ", err)
+	} else if matchHttps {
+		t.Error("Test failed: Response contains HTTPS URL")
+	}
+
+	// Check for protocol version ex: HTTP/1.1
+	matchProtVer, err := regexp.MatchString(`HTTP\/\d\.\d \d{3} [A-Z]*`, string(res))
+	if err != nil {
+		t.Fatal("Regexp crashed with unexpected error: ", err)
+	} else if !matchProtVer {
+		t.Error("Test failed: Response does not contain protocol version")
+	}
+}
